16029c88c91edb9e932f50e163d2c457b6d95f99
stephanie pi <stephmpi@gmail.com>
2014-09-03 11:32:46 -0700

    I think I added some stuff to associate topics with votes?

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903175744) do
+ActiveRecord::Schema.define(version: 20140903182843) do
 
   create_table "topics", force: true do |t|
     t.string   "title"
     t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "votes", force: true do |t|
+    t.integer  "topic_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
