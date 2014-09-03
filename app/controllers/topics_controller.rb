035ce3cf9526d329e91cbc85eed15b873065eac1
stephanie pi <stephmpi@gmail.com>
2014-09-03 12:24:34 -0700

    added downvote feature

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -66,6 +66,12 @@ def upvote
   redirect_to(topics_path)
 end
 
+def downvote
+  @topic = Topic.find(params[:id])
+  @topic.votes.first.destroy
+  redirect_to(topics_path)
+end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_topic
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     member do
       post 'upvote'
     end
+    member do
+      post 'downvote'
+    end
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
